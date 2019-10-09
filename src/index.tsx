cb1748bd48e42fafec2d62f67df61eb597bae5d1
Lecce Nikolas - MRAS-US <n1600216@am.munichre.com>
2019-10-09 13:49:04 -0400

    drawer in index

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { CalendarPage } from './calendar/Calendar';
+import { NavigationDrawer } from './drawer/Drawer';
 import './index.css';
 import Login from './login/Login';
-import {CalendarPage} from './calendar/Calendar';
-import {NavigationDrawer} from './drawer/Drawer';
 import * as serviceWorker from './serviceWorker';
 export const calendarPath = "/calendar";
 export const loginPath = "/";
 
-const routing = (
+const Routing = () => {
   const [showNavigationDrawer, setShowNavigationDrawer] = useState(false);
   const openNavigationDrawer = () => {
     setShowNavigationDrawer(true);
@@ -17,15 +17,26 @@ const routing = (
   const closeNavigationDrawer = () => {
     setShowNavigationDrawer(false);
   }
-  <Router>
-    <div>
-      <Route exact path={loginPath} component={Login}/>
-      <Route path={calendarPath} component={CalendarPage}/>
-    </div>
-  </Router>
-)
 
-ReactDOM.render(routing, document.getElementById('root'));
+  return (
+    <div>
+      <NavigationDrawer 
+        open={showNavigationDrawer}
+        onClose={closeNavigationDrawer}
+        items={[{path:loginPath, name: "Login"}]}
+      />
+      <Router>
+        <div>
+          <Route exact path={loginPath} component={Login} />
+          <Route path={calendarPath} component={CalendarPage} />
+        </div>
+      </Router>
+    </div>
+  )
+
+}
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
