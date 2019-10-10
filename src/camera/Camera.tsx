8c1bb13d1fdccf394456a6d64eaa183574f411d2
Nikolas Lecce <nikolaslecce@Nikolass-MacBook-Pro.local>
2019-10-10 16:04:10 -0400

    added camera component, only uses camera

---
+++ src/camera/Camera.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Webcam from "react-webcam";
+
+export const CameraCapture = () => {
+
+
+  return(
+      <div>
+        <Webcam
+
+        />
+      </div>
+  )
+}
